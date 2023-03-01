3b2047543a4d61d2e5d3175cdc6d78edf43e858b
Matth Wednesday <q3dm0@yahoo.com>
2023-03-01 17:19:47 +0300

    начало работы, конфиги специально в суппозитории

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://test.loc/admin/');
+define('HTTP_CATALOG', 'http://test.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://test.loc/admin/');
+define('HTTPS_CATALOG', 'http://test.loc/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/OpenServer/domains/test.loc/admin/');
+define('DIR_SYSTEM', 'D:/OpenServer/domains/test.loc/system/');
+define('DIR_IMAGE', 'D:/OpenServer/domains/test.loc/image/');
+define('DIR_STORAGE', 'D:/OpenServer/domains/teststorage/');
+define('DIR_CATALOG', 'D:/OpenServer/domains/test.loc/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'test');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
