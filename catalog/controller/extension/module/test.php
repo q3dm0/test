2e33d68e9d1f2c97a9932279234fc2aa0470f94c
Matth Wednesday <q3dm0@yahoo.com>
2023-03-02 11:50:17 +0300

    Конец работы, бд в корне test.sql

---
+++ catalog/controller/extension/module/test.php
@@ -0,0 +1,142 @@
+<?php
+//Работа q3dm0, использование только для просмотра навыков! 
+class ControllerExtensionModuleTest extends Controller {
+	public function index($setting) {
+		//$this->load->language('extension/module/featured');
+
+		$this->load->model('extension/module/test');
+
+		$this->load->model('tool/image');
+
+		$data['latest_month'] = array();
+		
+		$data['kick_ass_if_you_use'] = array();
+		
+		$filter_data = array(
+			/*'start'              => 1,
+			'limit'              => 8*/
+		);
+	
+		$latest_month = $this->model_extension_module_test->getProductsMonth($filter_data);
+		
+		if (!empty($latest_month)) {
+			$products = array_slice($latest_month, 0, 8);
+
+			foreach ($products as $product) {
+				$product_info = $this->model_catalog_product->getProduct($product['product_id']);
+
+				if ($product_info) {
+					if ($product_info['image']) {
+						$image = $this->model_tool_image->resize($product_info['image'], $setting['width'], $setting['height']);
+					} else {
+						$image = $this->model_tool_image->resize('placeholder.png', $setting['width'], $setting['height']);
+					}
+
+					if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
+						$price = $this->currency->format($this->tax->calculate($product_info['price'], $product_info['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+					} else {
+						$price = false;
+					}
+
+					if (!is_null($product_info['special']) && (float)$product_info['special'] >= 0) {
+						$special = $this->currency->format($this->tax->calculate($product_info['special'], $product_info['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+						$tax_price = (float)$product_info['special'];
+					} else {
+						$special = false;
+						$tax_price = (float)$product_info['price'];
+					}
+		
+					if ($this->config->get('config_tax')) {
+						$tax = $this->currency->format($tax_price, $this->session->data['currency']);
+					} else {
+						$tax = false;
+					}
+
+					if ($this->config->get('config_review_status')) {
+						$rating = $product_info['rating'];
+					} else {
+						$rating = false;
+					}
+
+					$data['latest_month'][] = array(
+						'product_id'  => $product_info['product_id'],
+						'thumb'       => $image,
+						'name'        => $product_info['name'],
+						'description' => utf8_substr(strip_tags(html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
+						'price'       => $price,
+						'special'     => $special,
+						'tax'         => $tax,
+						'rating'      => $rating,
+						'href'        => $this->url->link('product/product', 'product_id=' . $product_info['product_id'])
+					);
+				}
+			}
+		}
+		
+		$filter_data_two = array(
+			/*'start'              => 1,
+			'limit'              => 8*/
+		);
+	
+		$kick_ass_if_you_use = $this->model_extension_module_test->getProductsKick($filter_data_two);
+				
+		if (!empty($kick_ass_if_you_use)) {
+			$products = array_slice($kick_ass_if_you_use, 0, 8);
+
+			foreach ($products as $product) {
+				$product_info = $this->model_catalog_product->getProduct($product['product_id']);
+
+				if ($product_info) {
+					if ($product_info['image']) {
+						$image = $this->model_tool_image->resize($product_info['image'], $setting['width'], $setting['height']);
+					} else {
+						$image = $this->model_tool_image->resize('placeholder.png', $setting['width'], $setting['height']);
+					}
+
+					if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
+						$price = $this->currency->format($this->tax->calculate($product_info['price'], $product_info['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+					} else {
+						$price = false;
+					}
+
+					if (!is_null($product_info['special']) && (float)$product_info['special'] >= 0) {
+						$special = $this->currency->format($this->tax->calculate($product_info['special'], $product_info['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+						$tax_price = (float)$product_info['special'];
+					} else {
+						$special = false;
+						$tax_price = (float)$product_info['price'];
+					}
+		
+					if ($this->config->get('config_tax')) {
+						$tax = $this->currency->format($tax_price, $this->session->data['currency']);
+					} else {
+						$tax = false;
+					}
+
+					if ($this->config->get('config_review_status')) {
+						$rating = $product_info['rating'];
+					} else {
+						$rating = false;
+					}
+
+					$data['kick_ass_if_you_use'][] = array(
+						'product_id'  => $product_info['product_id'],
+						'thumb'       => $image,
+						'name'        => $product_info['name'],
+						'description' => utf8_substr(strip_tags(html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
+						'price'       => $price,
+						'special'     => $special,
+						'tax'         => $tax,
+						'rating'      => $rating,
+						'href'        => $this->url->link('product/product', 'product_id=' . $product_info['product_id'])
+					);
+				}
+			}
+		}
+
+		
+		if ($data['latest_month'] || $data['kick_ass_if_you_use']) {
+			return $this->load->view('extension/module/test', $data);
+		}
+	}
+}
